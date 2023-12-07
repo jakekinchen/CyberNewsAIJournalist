637407298697035c84f3f22d48758092f17fcc90
jakebuddy7 <65054338+jakebuddy7@users.noreply.github.com>
2023-12-07 12:35:29 -0600

    prompt shifting, removing context diluters

--- scripts/prompts.py
+++ scripts/prompts.py
@@ -2,20 +2,17 @@ synthesis = """
 You are an adept HTML writer at the New York Times, specializing in synthesizing multifaceted news articles. Your assignment is to merge the provided news articles into a singular, coherent news piece exceeding 350 words, focused on the overarching theme of Cybersecurity.
 
 Guidelines:
-Content: Develop a compelling and informative story, grounded in facts. Utilize active voice predominantly, aiming for at least 90% usage throughout the article.
+Content: Develop a compelling and informative story, grounded in facts. Utilize active voice predominantly throughout the article.
 Structure & Formatting:
 Embed spacious and readable spacing between paragraphs and pivotal sentences for enhanced dramatic effect.
-Indent the first sentence of every paragraph.
-Integrate a plethora of transition words to ensure fluidity.
-Include a prominent title at the beginning.
+Integrate a large amount of transition words to ensure fluidity.
 
 Relevance & Cohesion:
 Sift through the provided information, discarding any irrelevant details and amalgamating pertinent data into a unified narrative.
-Assure a coherent linkage between each story related to Cybersecurity, but do not use sentences longer than 15 words. Use shorter sentences where possible. Use a lot of transition words to ensure fluidity. Make sure to cite sources using <a href='https://www.citeyoursources'>the a tag format</a>.
-Here is an example: Reporters found a link between the letter and the <a href=https://www.reputable-source.com>attack</a>.
+Assure a coherent linkage between each story related to Cybersecurity, but do not use sentences longer than 15 words. Use shorter sentences where possible. Use a lot of transition words to ensure fluidity.
 
 Objective:
-Craft a seamless narrative on Cybersecurity, harmonizing diverse articles while adhering to the prescribed guidelines, and emphasizing readability, relevance, and dramatic appeal. Do not use the passive voice. Use lots of transition words, and try to use at least 500 words in total. Make sure the paragraphs are very short so it is very easy to read please, this is very important."""
+Craft a seamless narrative given the sources, harmonizing diverse articles while adhering to the prescribed guidelines, and emphasizing readability, relevance, and dramatic appeal. Do not use the passive voice. Use lots of transition words, and try to use at least 500 words in total. Make sure the paragraphs are very short so it is very easy to read, this is very important."""
 
 tech_term_prompt="You are a tech term source selector that selects the source that would be best for defining the tech term. This source should be the most relevant to the tech term and should be the most concise."
 
